830150f766db49292c39a20ebe769d821b9825c3
Patrick Coxall <patrick@coxall.ca>
2017-09-14 23:19:21 -0400

    init

---
+++ Contents/Chapters/Answers.playgroundchapter/Pages/Answer.playgroundpage/Contents.swift
@@ -0,0 +1,6 @@
+// Created on: Jan-2017
+// Created by: Mr. Coxall
+// Created for: ICS3U
+// This program is the solution for the Hello, World! program
+
+show("Hello, World!")
---
+++ Contents/Chapters/Answers.playgroundchapter/Pages/UiKit.playgroundpage/Contents.swift
@@ -0,0 +1,40 @@
+// Created on: Jan-2017
+// Created by: Mr. Coxall
+// Created for: ICS3U
+// This program is the UIKit solution for the Hello, World! program
+
+// this will be commented out when code moved to Xcode
+import PlaygroundSupport
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    // this is the main view controller, that will show the UIKit elements
+    
+    // properties
+    var helloWorldLabel : UILabel!
+    
+    override func viewDidLoad() {
+        // UI
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view = view
+        
+        helloWorldLabel = UILabel()
+        helloWorldLabel.text = "Hello, World!"
+        view.addSubview(helloWorldLabel)
+        helloWorldLabel.translatesAutoresizingMaskIntoConstraints = false
+        helloWorldLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        helloWorldLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+// this will be commented out when code moved to Xcode
+PlaygroundPage.current.liveView = ViewController()
